203cc6d0ccf180c4716d34911718f80c565f810f
Julio Amancio <julioamancio2014@gmail.com>
2026-07-30 21:22:59 -0300

    Jogo em tela cheia (resolucao nativa do monitor)

--- client/conf.lua
+++ client/conf.lua
@@ -1,9 +1,8 @@
 function love.conf(t)
     t.identity = "aldruna"
     t.window.title = "Aldruna"
-    t.window.width = 960
-    t.window.height = 704
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop" -- uses the player's native resolution
     t.window.vsync = 1
-    t.window.resizable = false
     t.console = false
 end
